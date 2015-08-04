49dd1aef10c3f8ebf98a621a436382e9e073ff55
Alexander Rhett Crammer <alexander2475914@gmail.com>
2015-08-04 18:43:29 -0400

    Finish input catching

---
+++ Mad Lib/main.py
@@ -0,0 +1,19 @@
+'''
+Mad Lib
+Tuesday, 4 August, 2015
+Alexander Rhett Crammer
+Full Sail University
+'''
+
+place = input("What's your favourite place to walk through?\n~> ")
+things = input("What do you carry when you walk?\n~> ").lower()
+giant_object = input("Largest object you can think of that stands:\n~> ").lower()
+action = input("What does that object do?\n~> ").lower()
+amount_of_times = int(input("Favourite number:\n~> "))
+time_of_day = input("Favourite time of day:\n~> ").lower()
+calling_code = int(input("What's your area code?\n~> "))
+grocery_item = input("Favourite foodstuff:\n~> ").lower()
+person = input("First name of your favourite person:\n~> ").lower()
+street_name = input("Favourite street name:\n~> ").lower()
+second_action = input("Something bad that you shouldn't do:\n~> ").lower()
+age = int(input("Age:\n~> "))
