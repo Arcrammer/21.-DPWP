6ef2f7607cb30e74f634b5c9499af928d41640cd
Alexander Rhett Crammer <alexander2475914@gmail.com>
2015-08-04 11:27:12 -0400

    Complete 'Comments, Variables, and Printing in Python'

--- Intro to Python/main.py
+++ Intro to Python/main.py
@@ -1,1 +1,33 @@
-print("Hello, world!")
+'''
+main.py
+Intro to Python
+Tuesday, 4 August, 2015
+Alexander Rhett Crammer
+Full Sail University
+
+This was written for Python
+version 3.4.3 which was
+installed on my Mac at the
+beginning of this course.
+I don't know how to downgrade
+but I've read that it's backwards
+compatible with 2.7.
+'''
+
+# print("Hello, world!")
+
+# Some properties
+first_name = "Alexander"
+middle_name = "Rhett"
+last_name = "Crammer"
+
+# Let's write those out
+# print(first_name)
+
+# response = input("What's your first name?\n") # raw_input() was renamed to input() in version 3.1 or so
+# print("Hello, %s!" % response)
+
+birth_year = 1995
+current_year = 2015
+year_difference = current_year - birth_year
+# print("You should be around " + str(year_difference) + " years of age.")
