e898177e9d710123a59cf86222d4ac9f6dd0f7a1
Alexander Rhett Crammer <alexander2475914@gmail.com>
2015-08-05 11:56:00 -0400

    Add Comments Detailing Functionality

--- Mad Lib/main.py
+++ Mad Lib/main.py
@@ -5,6 +5,7 @@ Alexander Rhett Crammer
 Full Sail University
 '''
 
+# Ask a series of questions and store the answers for them
 place = input("What's your favourite place to walk through?\n~> ")
 things = input("What do you carry when you walk?\n~> ").lower()
 giant_object = input("Largest object you can think of that stands:\n~> ").lower()
@@ -19,11 +20,19 @@ second_action = input("Something bad that you shouldn't do:\n~> ").lower()
 age = int(input("Age:\n~> "))
 os = input("'Windows', 'OS X', or 'Ubuntu'?\n~> ")
 
-if age > 20:
+age -= 10 # Subtract 10 to the value of the age
+calling_code //= amount_of_times # Reassign the value of 'calling_code' to
+# the floored division value of 'amount_of_times'
+
+# Set the value of 'old_or_young' with an if
+if age > 30:
 	old_or_young = "old"
+elif age >= 20 || age == 21:
+	old_or_young = "going old"
 else:
 	old_or_young = "young"
 
+# Dictionary of OS data
 operating_systems = {
 	"Windows": {
 		"Creator":"Microsoft",
@@ -53,8 +62,8 @@ else:
 story = '''
 I was walking through {place} with my {things} when I had suddenly noticed a giant {giant_object} directly in my path. It was seemingly inanimate and unaware of my presence so it continued to {action} without a thought. This particular {giant_object} appeared to be {old_or_young}. It was the most giant {giant_object} within the {calling_code} area code. Why would such a significant and majestic {giant_object} choose to {action} ever-so blissfully unaware of the world around itself? Later that same {time_of_day} I had decided to visit the grocery store because I had completely run out of {grocery_item} the day before -- {person} knows I can't go without my precious {grocery_item}s. However... What I had anticipated to be a lovely shop for {grocery_item}s had turned out to be a nightmare on {street_name} street. Ah! You gave me a fright, sir {person}. My life was forever changed on that day. I decide to {second_action}. I've been told {amount_of_times} times not to do that or I would be arrested. I don't see why it's illegal, but I was still forewarned not to do it again. Never again shall I {second_action}! I must now update my {os_creator} computer. It's still running OS version {os_version}!
 '''
-print(story.format(**locals()))
+print(story.format(**locals())) # Write out the story
 colours = ["Red","Orange","Yellow","Green","Teal","Blue","Purple","Magenta"]
 print("Which colours are to be found in a rainbow? These:")
-for colour in colours:
-	print(colour)
+for colour in colours: # For each colour in the array of colours
+	print(colour) # Print the colours' name
