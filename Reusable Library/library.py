9cc917db871ba58212b6b7374eb69018cbdc6da0
Alexander Rhett Crammer <alexander2475914@gmail.com>
2015-08-14 16:41:26 -0400

    Begin 'Reusable Library'

---
+++ Reusable Library/library.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# --------------------------
+#   library.py
+#   Reusable Library
+#   Friday, 14 August, 2015
+#   Alexander Rhett Crammer
+#   Full Sail University
+# --------------------------
+#
+class ClassName(object):
+    def __init__(self):
+        pass
---
+++ Reusable Library/pages.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# --------------------------
+#   pages.py
+#   Reusable Library
+#   Friday, 14 August, 2015
+#   Alexander Rhett Crammer
+#   Full Sail University
+# --------------------------
+#
+def Page(object):
+    def __init__(self):
+        pass
