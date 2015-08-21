0cbd08b4f9f0e4eab5179cf6be0654ec811c67d2
Alexander Rhett Crammer <alexander2475914@gmail.com>
2015-08-21 10:10:52 -0400

    Begin 'Dynamic Site'

---
+++ Dynamic Site/data.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# --------------------------
+#   data.py
+#   Dynamic Site
+#   Friday, 21 August, 2015
+#   Alexander Rhett Crammer
+#   Full Sail University
+# --------------------------
+#
