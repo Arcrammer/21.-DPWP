52d962a58cc0dc4f048d7d82e1de4b51856cf497
Alexander Rhett Crammer <alexander2475914@gmail.com>
2015-08-21 10:50:30 -0400

    Make Views Class Based
    Views are chosen based on the object instantiated. Page() returns
    ‘Welcome.html’ and ContentPage() returns ‘Content.html’.
    
    The view name can be changed after instantiation, too.

--- Dynamic Site/pages.py
+++ Dynamic Site/pages.py
@@ -9,12 +9,11 @@
 #
 class Page(object):
     def __init__(self):
-        pass
-
-    def render_view(self, desired_view_name):
-        ''' Return the view to the client '''
         self._title = "Welcome" # Title of this page
-        self._view_name = desired_view_name
+        self._view_name = "Welcome" # Seemingly pointless name of the view; Allow other classes to redefine the desired views' name
+
+    def render_view(self):
+        ''' Return the view to the client '''
         self._view = open(str("Pages/{view_name}.html").format(view_name=self._view_name)) # Open the template
         return self._view.read().format(**locals()) # Read the template, interpolate local variables in the data, then return it
 
@@ -30,4 +29,5 @@ class Page(object):
 
 class ContentPage(Page):
     def __init__(self):
-        pass
+        self._title = "Content Page"
+        self._view_name = "Content"
