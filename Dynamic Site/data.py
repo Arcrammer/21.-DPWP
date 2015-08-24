af5fbc4ebf48427782eab004897a5c086e55997f
Alexander Rhett Crammer <alexander2475914@gmail.com>
2015-08-24 10:58:48 -0400

    WAIT! Now they're protected... Sorry...
    I don’t understand what just happened.
    
    After that last commit some of the pages would load correctly and
    others would tell me I had no such property as ‘title’ (in the view
    template).
    
    Moving on!

--- Dynamic Site/data.py
+++ Dynamic Site/data.py
@@ -33,45 +33,45 @@ class Work(object):
         ''' Set the appropriate properties to self._art '''
         self._art_name = new_art_name
         if self._art_name == "Mona-Lisa":
-            self._art.title = self._art_name
-            self._art.artist = "Leonardo da Vinci"
-            self._art.time = "Early 1500's"
-            self._art.description = "Portrait of Lisa Gherardini, wife of Francesco del Giocondo, referred to as Monna Lisa, The Gioconda or The Joconde. Painted in Florence, Italy in the early 1500's."
-            self._art.current_location = "La Louvre, Paris"
-            self._art.materials_used = "Oil Paint on Poplar Wood"
-            self._art.image_name = "MonaLisa.jpg"
+            self._art._title = self._art_name
+            self._art._artist = "Leonardo da Vinci"
+            self._art._time = "Early 1500's"
+            self._art._description = "Portrait of Lisa Gherardini, wife of Francesco del Giocondo, referred to as Monna Lisa, The Gioconda or The Joconde. Painted in Florence, Italy in the early 1500's."
+            self._art._current_location = "La Louvre, Paris"
+            self._art._materials_used = "Oil Paint on Poplar Wood"
+            self._art._image_name = "MonaLisa.jpg"
         elif self._art_name == "Birth-of-Venus":
-            self._art.title = self._art_name
-            self._art.artist = "Sandro Botticelli"
-            self._art.time = "Circa 1485"
-            self._art.description = "Undoubtedly the most well known and appreciated work of Italy's 15th century, this work follows the theme from Ovid's Metamorphoses."
-            self._art.current_location = "Uffizi Gallery, Florence"
-            self._art.materials_used = "Tempera on Canvas"
-            self._art.image_name = "BirthOfVenus.jpg"
+            self._art._title = self._art_name
+            self._art._artist = "Sandro Botticelli"
+            self._art._time = "Circa 1485"
+            self._art._description = "Undoubtedly the most well known and appreciated work of Italy's 15th century, this work follows the theme from Ovid's Metamorphoses."
+            self._art._current_location = "Uffizi Gallery, Florence"
+            self._art._materials_used = "Tempera on Canvas"
+            self._art._image_name = "BirthOfVenus.jpg"
         elif self._art_name == "Starry-Night":
             self._art._title = self._art_name
-            self._art.artist = "Vincent van Gogh"
-            self._art.time = "Circa 1889"
-            self._art.description = "Originally titled &quot;La Nuit Entoill&eacute;&quot;, this is pherhaps Van Gogh's most well known rendition of the view from his asylum room at Saint-R&eacute;my-de-Provence."
-            self._art.current_location = "Museum of Modern Art, New York"
-            self._art.materials_used = "Oil on Canvas"
-            self._art.image_name = "StarryNight.jpg"
+            self._art._artist = "Vincent van Gogh"
+            self._art._time = "Circa 1889"
+            self._art._description = "Originally titled &quot;La Nuit Entoill&eacute;&quot;, this is pherhaps Van Gogh's most well known rendition of the view from his asylum room at Saint-R&eacute;my-de-Provence."
+            self._art._current_location = "Museum of Modern Art, New York"
+            self._art._materials_used = "Oil on Canvas"
+            self._art._image_name = "StarryNight.jpg"
         elif self._art_name == "Persistence-of-Memory":
             self._art._title = self._art_name
-            self._art.artist = "Salvador Dal&#xED;"
-            self._art.time = "1931"
-            self._art.description = "Dal&#xED; used oil on canvas to portray his perception of softness and hardness. He'd often used ants to resemble death and rotting."
-            self._art.current_location = "Museum of Modern Art, New York"
-            self._art.materials_used = "Oil on Canvas"
-            self._art.image_name = "PresistenceOfMemory.jpg"
+            self._art._artist = "Salvador Dal&#xED;"
+            self._art._time = "1931"
+            self._art._description = "Dal&#xED; used oil on canvas to portray his perception of softness and hardness. He'd often used ants to resemble death and rotting."
+            self._art._current_location = "Museum of Modern Art, New York"
+            self._art._materials_used = "Oil on Canvas"
+            self._art._image_name = "PresistenceOfMemory.jpg"
         elif self._art_name == "Girl-With-a-Pearl-Earring":
             self._art._title = self._art_name
-            self._art.artist = "Johannes Vermeer"
-            self._art.time = "1665"
-            self._art.description = "Tronie by Johannes Vermeer created 1665."
-            self._art.current_location = "Mauritshuis, The Hague"
-            self._art.materials_used = "Oil on Canvas"
-            self._art.image_name = "GirlWithPearlEarring.jpg"
+            self._art._artist = "Johannes Vermeer"
+            self._art._time = "1665"
+            self._art._description = "Tronie by Johannes Vermeer created 1665."
+            self._art._current_location = "Mauritshuis, The Hague"
+            self._art._materials_used = "Oil on Canvas"
+            self._art._image_name = "GirlWithPearlEarring.jpg"
 
 class Art(Work):
     def __init__(self):
