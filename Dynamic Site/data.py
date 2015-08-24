b21b3e26ed96f2808b0704ccb67b286ac2c089c7
Alexander Rhett Crammer <alexander2475914@gmail.com>
2015-08-24 10:19:37 -0400

    Move Art() Object Instantiation
    Instantiate and Set Art() in Work() (Model) Rather Than MainHandler
    (Controller)

--- Dynamic Site/data.py
+++ Dynamic Site/data.py
@@ -6,7 +6,7 @@
 #   Alexander Rhett Crammer
 #   Full Sail University
 # --------------------------
-#
+# MODELS
 class Work(object):
     # IMPORTANT: There should be five data objects which
     #   should be hard coded in _this_ object. They should
@@ -15,6 +15,58 @@ class Work(object):
     def __init__(self):
         self._title = None # Title of the work
         self._time = None # String describing the time of the works' completion
+        self._art_name = None # Name of the art object
+        self._art = Art() # 'Art' requested
+
+    # Getters
+    @property
+    def art_name(self):
+        return self._art_name
+
+    @property
+    def art(self):
+        return self._art
+
+    # Setters
+    @art_name.setter
+    def art_name(self, new_art_name):
+        ''' Set the appropriate properties to self._art '''
+        if self._art_name == "Mona-Lisa":
+            self._art.artist = "Leonardo da Vinci"
+            self._art.time = "Early 1500's"
+            self._art.description = "Portrait of Lisa Gherardini, wife of Francesco del Giocondo, referred to as Monna Lisa, The Gioconda or The Joconde. Painted in Florence, Italy in the early 1500's."
+            self._art.current_location = "La Louvre, Paris"
+            self._art.materials_used = "Oil Paint on Poplar Wood"
+            self._art.image_name = "MonaLisa.jpg"
+        elif self._art == "Birth-of-Venus":
+            self._art.artist = "Sandro Botticelli"
+            self._art.time = "Circa 1485"
+            self._art.description = "Undoubtedly the most well known and appreciated work of Italy's 15th century, this work follows the theme from Ovid's Metamorphoses."
+            self._art.current_location = "Uffizi Gallery, Florence"
+            self._art.materials_used = "Tempera on Canvas"
+            self._art.image_name = "BirthOfVenus.jpg"
+        elif self._art == "Starry-Night":
+            self._art.artist = "Vincent van Gogh"
+            self._art.time = "Circa 1889"
+            self._art.description = "Originally titled &quot;La Nuit Entoill&eacute;&quot;, this is pherhaps Van Gogh's most well known rendition of the view from his asylum room at Saint-R&eacute;my-de-Provence."
+            self._art.current_location = "Museum of Modern Art, New York"
+            self._art.materials_used = "Oil on Canvas"
+            self._art.image_name = "StarryNight.jpg"
+        elif self._art == "Persistence-of-Memory":
+            self._art.artist = "Salvador Dal&#xED;"
+            self._art.time = "1931"
+            self._art.description = "Dal&#xED; used oil on canvas to portray his perception of softness and hardness. He'd often used ants to resemble death and rotting."
+            self._art.current_location = "Museum of Modern Art, New York"
+            self._art.materials_used = "Oil on Canvas"
+            self._art.image_name = "PresistenceOfMemory.jpg"
+        elif self._art == "Girl-With-a-Pearl-Earring":
+            self._art.artist = "Johannes Vermeer"
+            self._art.time = "1665"
+            self._art.description = "Tronie by Johannes Vermeer created 1665."
+            self._art.current_location = "Mauritshuis, The Hague"
+            self._art.materials_used = "Oil on Canvas"
+            self._art.image_name = "GirlWithPearlEarring.jpg"
+        self._art_name = new_art_name
 
 class Art(Work):
     def __init__(self):
--- Dynamic Site/pages.py
+++ Dynamic Site/pages.py
@@ -6,14 +6,14 @@
 #   Alexander Rhett Crammer
 #   Full Sail University
 # --------------------------
-#
+# VIEWS
 class Page(object):
     def __init__(self):
         self._title = None # Title of this page
 
     def render_view(self):
         ''' Return the view to the client '''
-        return open("Pages/Credit.html").read() # Read the template then return it
+        return open("Pages/Credit.html").read().format(**locals()) # Read the template, interpolate local variable values, then return it
 
     # Getters
     @property
@@ -30,7 +30,7 @@ class FamousWorkPage(Page):
         self._art = None # Art objects for the page
 
     def render_view(self):
-        # Render the view with properties local to this object
+        ''' Render the view with properties local to this object '''
         return open("Pages/Content.html").read().format(**locals()) # Open the template, read the page to a string, then interpolate this instances' properties and return it
 
     # Getters
