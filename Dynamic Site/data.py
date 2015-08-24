ea91ed7f89182f11aee9223280b7e0c0a4a3fb64
Alexander Rhett Crammer <alexander2475914@gmail.com>
2015-08-24 15:11:50 -0400

    Update Comments; Add TODOs

--- Dynamic Site/data.py
+++ Dynamic Site/data.py
@@ -30,8 +30,9 @@ class Work(object):
     # Setters
     @art_name.setter
     def art_name(self, new_art_name):
-        ''' Set the appropriate properties to self._art '''
+        ''' Set the name of the work requested; Set the appropriate properties to self._art '''
         self._art_name = new_art_name
+        # TODO: Show all of these seven properties in the view
         if self._art_name == "Mona-Lisa":
             self._art._title = self._art_name
             self._art._artist = "Leonardo da Vinci"
